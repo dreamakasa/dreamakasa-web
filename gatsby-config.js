b48f04f5b48ae40c8c3b6d20f34b94408f9cbf6f
Setyo Nugroho <setyo.nugroho1337@gmail.com>
2017-09-18 00:43:23 +0700

    Add new plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Dream Akasa`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-nprogress`],
 }
