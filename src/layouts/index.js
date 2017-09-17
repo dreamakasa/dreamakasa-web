cad06b6430815f9e76681ef8576c72de64378a29
Setyo Nugroho <setyo.nugroho1337@gmail.com>
2017-09-17 22:52:13 +0700

    Change index layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,57 +3,47 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import './index.css'
-
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
+  <nav className="navbar">
+    <div className="container-fluid">
+          <div className="navbar__header">
+              <a href="/" id="logo" data-toggle="tooltip" data-placement="bottom" title="Dream Akasa">
+                  <img src="images/logo.svg" alt="Dream Akasa" />
+              </a>
+              <button type="button" className="btn toggle-menu pull-right" id="toggleNavbar">
+                  <span className="bar">
+                      <span className="icon-bar"></span>
+                      <span className="icon-bar"></span>
+                      <span className="icon-bar"></span>
+                  </span>
+                  <span className="text">Menu</span>
+              </button>
+          </div>
+          <div className="navbar__collapse">
+        <ul className="nav navbar-nav pull-left">
+          <li className=""><a href="index.html">Home</a></li>
+          <li className=""><a href="about.html">About</a></li>
+        </ul>
+        <ul className="nav navbar-nav pull-right">
+          <li className=""><a href="services.html">Services</a></li>
+          <li className=""><a href="contact.html">Get In Touch</a></li>
+        </ul>
+          </div>
     </div>
-  </div>
+  </nav>
 )
 
 const TemplateWrapper = ({ children }) => (
-  <div>
+  <div className="wrapper">
     <Helmet
-      title="Gatsby Default Starter"
+      title="Dream Akasa | We help companies from design to development with solution-by-design and human-centered approach."
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'Dream Akasa' },
+        { name: 'keywords', content: 'dreamakasa, Dream Akasa, development' },
       ]}
     />
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    {children()}
   </div>
 )
 
